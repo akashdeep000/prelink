bd7099985cad691153512b0863203bf7a363886a
AkashDeep000 <akashdeepdas970@gmail.com>
2022-08-08 10:41:12 +0530

    added pogress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
 import "../styles/globals.css";
-import Script from "next/script";
+import NextNProgress from "nextjs-progressbar";
+
 function MyApp({ Component, pageProps }) {
   return (
     <>
-
+      <NextNProgress
+        options={{
+          showSpinner: false,
+        }}
+        color="#6366f1"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+        showOnShallow={true}
+      />
       <Component {...pageProps} />
     </>
   );
